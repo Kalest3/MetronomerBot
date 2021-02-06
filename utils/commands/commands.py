65b8f835319191ae55bec10941d34a0c6edca48e
Kalest3 <gatchmew@gmail.com>
2021-02-06 16:16:52 -0300

    Small changes

--- utils/commands/commands.py
+++ utils/commands/commands.py
@@ -1,5 +1,6 @@
+from config import username
 def spheal(user):
    return f'|/pm {user}, spheal'
 async def runall(msg, websocket, userSearch):
-   if msg == f'|pm| {userSearch}| MetronomerBot|@spheal':
+   if msg == f'|pm| {userSearch}| {username}|@spheal':
       await websocket.send(spheal(user=userSearch))
