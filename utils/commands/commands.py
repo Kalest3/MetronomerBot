f1d514b9c236b0eb5f173fc52b579a76e4fa4794
Kalest3 <gatchmew@gmail.com>
2021-02-06 22:58:56 -0300

    Fix command bug

--- utils/commands/commands.py
+++ utils/commands/commands.py
@@ -2,5 +2,5 @@ from config import username
 def spheal(user):
    return f'|/pm {user}, spheal'
 async def runall(msg, websocket, userSearch):
-   if msg == f'|pm| {userSearch}| {username}|@spheal':
+   if msg.replace(' ', '') == f'|pm|{userSearch}|{username}|@spheal':
       await websocket.send(spheal(user=userSearch))
