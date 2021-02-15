69d9bf3f4984376c92e19e9f233daca6d3f62ccc
Kalest3 <gatchmew@gmail.com>
2021-02-15 16:35:45 -0300

    Fix typo

--- utils/commands/commands.py
+++ utils/commands/commands.py
@@ -4,7 +4,7 @@ def PM(userSearch, command: str):
 def spheal(user: str):
    return f'|/pm {user}, spheal'
 def commands(user: str):
-   return f'|/pm {user}, See my commands in: https://github.com/Kalest3/MetronomerBot/blob/master/README.md#commands'
+   return f'|/pm {user}, See my commands at: https://github.com/Kalest3/MetronomerBot/blob/master/README.md#commands'
 async def runall(msg, websocket, userSearch):
    if msg.replace(' ', '').lower() == PM(userSearch, 'spheal'):
       await websocket.send(spheal(user=userSearch))
